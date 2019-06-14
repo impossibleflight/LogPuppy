e444320fe58b80e0e6fb5cf479ac63acd7ff2afb
John Clayton <john@fivesquare.net>
2019-06-14 14:32:08 -0400

    Provide a public protocol for logging so users can extend or redirect logging interfaces

--- Sources/LogPuppy/Logger.swift
+++ Sources/LogPuppy/Logger.swift
@@ -8,10 +8,46 @@
 
 import Foundation
 
-public final class Logger {
+public typealias MeasurementBeacon = ()->Void
+
+public protocol Logging {
+	func log(_ levels: Level, _ format: String, dso: UnsafeRawPointer, file: String, function: String, line: Int, column: Int)
+	func log(_ levels: Level, _ format: String, dso: UnsafeRawPointer, file: String, function: String, line: Int, column: Int, _ args: CVarArg...)
+	func log(_ levels: Level, _ format: String, dso: UnsafeRawPointer, file: String, function: String, line: Int, column: Int, arguments: [CVarArg])
+}
+
+public protocol ConvenienceLogging {
+	func `default`(_ message: String, dso: UnsafeRawPointer, file: String, function: String, line: Int, column: Int, _ args: CVarArg...)
+	func info(_ message: String, dso: UnsafeRawPointer, file: String, function: String, line: Int, column: Int, _ args: CVarArg...)
+	func debug(_ message: String, dso: UnsafeRawPointer, file: String, function: String, line: Int, column: Int, _ args: CVarArg...)
+	func error(_ message: String, dso: UnsafeRawPointer, file: String, function: String, line: Int, column: Int, _ args: CVarArg...)
+	func fault(_ message: String, dso: UnsafeRawPointer, file: String, function: String, line: Int, column: Int, _ args: CVarArg...)
+
+	func `default`(_ message: String, dso: UnsafeRawPointer, file: String, function: String, line: Int, column: Int)
+	func info(_ message: String, dso: UnsafeRawPointer, file: String, function: String, line: Int, column: Int)
+	func debug(_ message: String, dso: UnsafeRawPointer, file: String, function: String, line: Int, column: Int)
+	func error(_ message: String, dso: UnsafeRawPointer, file: String, function: String, line: Int, column: Int)
+	func fault(_ message: String, dso: UnsafeRawPointer, file: String, function: String, line: Int, column: Int)
+}
+
+public protocol ErrorLogging {
+	func error(_ error: Error, dso: UnsafeRawPointer, file: String, function: String, line: Int, column: Int)
+}
+
+public protocol Benchmarking {
+	func measure(_ levels: Level, _ label: String, dso: UnsafeRawPointer, file: String, function: String, line: Int, column: Int, closure: ()->Void)
+	func measureAsync(_ levels: Level, _ label: String, dso: UnsafeRawPointer, file: String, function: String, line: Int, column: Int, closure: (@escaping MeasurementBeacon)->Void)
+	func timeBeacon(_ levels: Level, _ label: String, dso: UnsafeRawPointer, file: String, function: String, line: Int, column: Int) -> MeasurementBeacon
+}
+
+open class Logger: Logging {
 	public var destinations: [Destination]
 	public init(destinations: [Destination]) {
 		self.destinations = destinations
+	}
+
+	public func log(_ levels: Level = .default, _ format: String, dso: UnsafeRawPointer = #dsohandle, file: String = #file, function: String = #function, line: Int = #line, column: Int = #column) {
+		log(levels, format, dso: dso, file: file, function: function, line: line, column: column, arguments: [])
 	}
 
 	public func log(_ levels: Level = .default, _ format: String, dso: UnsafeRawPointer = #dsohandle, file: String = #file, function: String = #function, line: Int = #line, column: Int = #column, _ args: CVarArg...) {
@@ -37,7 +73,8 @@ public final class Logger {
 	private static var _debugLogger: Logger?
 }
 
-extension Logger {
+//MARK: ConvenienceLogging
+extension Logger: ConvenienceLogging {
 	public func `default`(_ format: String, dso: UnsafeRawPointer = #dsohandle, file: String = #file, function: String = #function, line: Int = #line, column: Int = #column, _ args: CVarArg...) {
 		log(.default, format, dso: dso, file: file, function: function, line: line, column: column, arguments: args)
 	}
@@ -53,9 +90,7 @@ extension Logger {
 	public func fault(_ format: String, dso: UnsafeRawPointer = #dsohandle, file: String = #file, function: String = #function, line: Int = #line, column: Int = #column, _ args: CVarArg...) {
 		log(.fault, format, dso: dso, file: file, function: function, line: line, column: column, arguments: args)
 	}
-}
 
-extension Logger {
 	public func `default`(_ message: String, dso: UnsafeRawPointer = #dsohandle, file: String = #file, function: String = #function, line: Int = #line, column: Int = #column) {
 		log(.default, message, dso: dso, file: file, function: function, line: line, column: column, arguments: [])
 	}
@@ -73,13 +108,15 @@ extension Logger {
 	}
 }
 
-extension Logger {
+//MARK: ErrorLogging
+extension Logger: ErrorLogging {
 	public func error(_ error: Error, dso: UnsafeRawPointer = #dsohandle, file: String = #file, function: String = #function, line: Int = #line, column: Int = #column) {
 		log(.error, "%@", dso: dso, file: file, function: function, line: line, column: column, arguments: [String(describing: error)])
 	}
 }
 
-extension Logger {
+//MARK: Benchmarking
+extension Logger: Benchmarking {
 	public func measure(_ levels: Level = .default, _ label: String = #function, dso: UnsafeRawPointer = #dsohandle, file: String = #file, function: String = #function, line: Int = #line, column: Int = #column, closure: ()->Void) {
 		let callback = timeBeacon(levels, label, file: file, function: function, line: line, column: column)
 		closure()
@@ -96,13 +133,12 @@ extension Logger {
 	///   - line: <#line description#>
 	///   - column: <#column description#>
 	///   - closure: <#closure description#>
-	public func measureAsync(_ levels: Level = .default, _ label: String = #function, dso: UnsafeRawPointer = #dsohandle, file: String = #file, function: String = #function, line: Int = #line, column: Int = #column, closure: (@escaping MeasumentBeacon)->Void) {
+	public func measureAsync(_ levels: Level = .default, _ label: String = #function, dso: UnsafeRawPointer = #dsohandle, file: String = #file, function: String = #function, line: Int = #line, column: Int = #column, closure: (@escaping MeasurementBeacon)->Void) {
 		let callback = timeBeacon(levels, label, file: file, function: function, line: line, column: column)
 		closure(callback)
 	}
 
-	public typealias MeasumentBeacon = ()->Void
-	public func timeBeacon(_ levels: Level = .default, _ label: String = #function, dso: UnsafeRawPointer = #dsohandle, file: String = #file, function: String = #function, line: Int = #line, column: Int = #column) -> MeasumentBeacon {
+	public func timeBeacon(_ levels: Level = .default, _ label: String = #function, dso: UnsafeRawPointer = #dsohandle, file: String = #file, function: String = #function, line: Int = #line, column: Int = #column) -> MeasurementBeacon {
 		let message = "Started { \(label) }"
 		log(levels, message, file: file, function: function, line: line, column: column, arguments: [])
 		let start = CFAbsoluteTimeGetCurrent()
